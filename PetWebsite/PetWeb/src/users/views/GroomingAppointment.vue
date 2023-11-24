bb1730715c8dd59b60833e12c969806dc54f81bd
zhixuan07 <69116120+divesh330@users.noreply.github.com>
2023-11-24 20:08:15 +0800

    Add the page for user select service and appointment form

---
+++ PetWebsite/PetWeb/src/users/views/GroomingAppointment.vue
@@ -0,0 +1,84 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+import {useRouter} from 'vue-router';
+import {ref} from 'vue';
+import { useServiceStore } from "../stores/service";
+const router = useRouter();
+const serviceStore = useServiceStore();
+const service = {
+   typeService:"",
+   appointmentDate:"",
+   appointmentTime:"",
+   typePet:"",
+   numberPet:""
+};
+
+
+const proceedCheckout = () => {
+  if(service.typeService == "" || service.appointmentDate == "" || service.appointmentTime == "" || service.typePet == "" || service.numberPet == ""){
+      alert("Please fill in all the information");
+   }else{
+       localStorage.setItem("service",JSON.stringify(service));
+       router.push("/groomingCheckout");
+   }
+     
+     
+  
+};
+
+
+
+
+</script>
+
+
+<template>
+   <Header></Header>
+   <div class="flex justify-center w-screen h-screen">
+       <div class="shadow-lg h-fit px-20 py-10">
+           <div class="">
+               <label class="label"> Type of Service </label>
+               <select class="select select-bordered" v-model="service.typeService">
+                   <option  >Pick one</option>
+                   <option value="Bath and brush">Bath and Brush-RM50</option>
+                   <option value="Haircut and Styling">
+                       Haircut and Styling-RM65
+                   </option>
+               </select>
+           </div>
+           <div class="flex flex-col">
+               <label>Date</label>
+               <input
+                   type="date"
+                   id="expiryDate"
+                   v-model="service.appointmentDate"
+                   class="bg-white py-2 px-1 rounded-lg border"
+               />
+               <label>Time</label>
+               <input
+                   type="time"
+                   id="expiryDate"
+                   v-model="service.appointmentTime"
+                   class="bg-white py-2 px-1 rounded-lg border"
+               />
+               <label>Type of pet</label>
+             <select class="bg-white py-2 px-2 rounded-lg border" v-model="service.typePet">
+               <option value="Dog">Dog</option>
+               <option value="Cat">Cat</option>
+             </select>
+             <label>Number of pet</label>
+             <input
+             type="number"
+           
+             v-model="service.numberPet"
+             class="bg-white py-2 px-1 rounded-lg border"/>
+             <button class="btn btn-secondary mt-3" @click="proceedCheckout">Submit</button>
+           </div>
+          
+
+
+       </div>
+   </div>
+   <Footer></Footer>
+</template>
---
+++ PetWebsite/PetWeb/src/users/views/Services.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="grooming">
+      <!-- Header Component -->
+      <Header />
+       <div class="grooming-page">
+          <h1 class="title">GROOMING SERVICES</h1>
+          <p class="service-comment">Select a service to book an appointment!</p>
+ 
+ 
+          <!-- List of Grooming Services -->
+          <div class="grooming-services">
+              <!-- You can loop through your grooming services data and display them as cards or a list -->
+              <div class="card" @click="bookBathService">
+                  <img class="card-img" src="../assets/bathservice.jpg" alt="Grooming Service Image">
+                  <p class="service-name">Bath and Brush</p>
+                  <p class="service-price">RM 50.00</p>
+              </div>
+              <div class="card" @click="bookStylingService">
+                  <img class="card-img" src="../assets/stylingservice.png" alt="Grooming Service Image">
+                  <p class="service-name">Haircut and Styling</p>
+                  <p class="service-price">RM 65.00</p>
+              </div>
+          </div>
+      </div>
+ 
+ 
+      <!-- Footer Component -->
+      <Footer />
+  </div>
+ </template>
+ 
+ 
+ <script>
+ import Header from '../components/Header.vue';
+ import Footer from '../components/Footer.vue';
+ 
+ 
+ export default {
+  components: {
+      Header,
+      Footer
+  },
+  methods: {
+      bookBathService() {
+          // Implement the logic to navigate to the booking page for the selected grooming service.
+          // You may use Vue Router or other navigation methods.
+          this.$router.push('/groomingAppointment'); // Navigate to the Bath Service page
+      },
+      bookStylingService() {
+          // Implement the logic to navigate to the booking page for the selected grooming service.
+          // You may use Vue Router or other navigation methods.
+          this.$router.push('/groomingAppointment'); // Navigate to the Bath Service page
+      },
+  },
+ };
+ </script>
+ 
+ 
+ <style scoped>
+ .grooming-page {
+  background-color: white;
+  color: black;
+  text-align: center;
+  padding: 20px;
+ }
+ 
+ 
+ .title {
+  font-size: 40px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-bottom: 35px;
+ }
+ 
+ 
+ .service-comment {
+  justify-content: center;
+  margin-bottom: 35px;
+ }
+ 
+ 
+ .grooming-services {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px; /* Adjust the gap between grooming service cards as desired */
+ }
+ 
+ 
+ .card {
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: center;
+  cursor: pointer;
+ }
+ 
+ 
+ .card-img {
+  height: 250px;
+  object-fit: cover;
+  margin: 10px;
+ }
+ 
+ 
+ .service-name {
+  font-weight: bold;
+ }
+ 
+ 
+ .service-price {
+  margin-top: 10px;
+ }
+ </style>
+ 
